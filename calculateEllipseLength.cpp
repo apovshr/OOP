aa1585d2d1c5d0f3a2e208fd646de83ca1141a58
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2026-04-02 11:58:43 -0600

    Файл для лаб 9

--- calculateEllipseLength.cpp
+++ calculateEllipseLength.cpp
@@ -1,7 +1,7 @@
 #include "Ellipse.h"
 #include <cmath>
 
-// метод рахунку довжини еліпса
+// метод рахунку довжини еліпса 
 double Ellipse :: calculateEllipseLength()
 {
     double length = 4 * (((M_PI * a * b) + (a - b)) / (a + b));
