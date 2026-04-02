4e6a0025b493498ba73445519ef4165638dce832
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2026-04-02 11:57:43 -0600

    Файл для лаб 9

--- calculateCircleLength.cpp
+++ calculateCircleLength.cpp
@@ -1,7 +1,7 @@
 #include "Circle.h"
 #include <cmath>
 
-// метод рахунку довжини кола
+// метод рахунку довжини кола 
 double Circle :: calculateCircleLength() const
 {
     double length = 2 * M_PI * r;
