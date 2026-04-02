41d1758106f55add3410661fba95953cba6794e4
Anastasiia Lytvynenko <152660862+apovshr@users.noreply.github.com>
2026-04-02 12:42:40 -0600

    Файл для лаб 8

--- distance.cpp
+++ distance.cpp
@@ -1,7 +1,7 @@
 #include "Point.h"
 #include <cmath>
 
-// метод для рахунку дистанції
+// метод для рахунку дистанції 
 void Point :: distance()
 {
     dis = sqrt(pow(coordinates[0], 2) + pow(coordinates[1], 2) + pow(coordinates[2], 2));
